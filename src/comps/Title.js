74784ad25d3021f507f977b458f9cc9c2a62f564
soubhagya-zeotap <soubhagya.mishra@zeotap.com>
2021-04-24 15:36:28 +0530

    initial and final commit of gallery

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      {/* <h1>FireGram</h1> */}
+      <h2 data-text="Gallery">Gallery</h2>
+      <p>A Collection to hold on to memories</p>
+    </div>
+  );
+};
+
+export default Title;
