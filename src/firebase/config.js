131853510fb58c7de5fd02500ef2500a75d3a60d
soubhagya-zeotap <soubhagya.mishra@zeotap.com>
2021-04-24 15:47:45 +0530

    reverted env changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ require("dotenv").config();
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDDv3IoL89T25ZN-3kDSRnpQZbyY4dRdUw",
+  authDomain: "firegram-15811.firebaseapp.com",
+  projectId: "firegram-15811",
+  storageBucket: "firegram-15811.appspot.com",
+  messagingSenderId: "63753284757",
+  appId: "1:63753284757:web:e897375b2e9af77ccc7f9c",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
